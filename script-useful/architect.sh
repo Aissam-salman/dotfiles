6cf88b940db747a6197fa5f29334efac2adce498
”Aissam-salman” <nasawi.lmj@gmail.com>
2025-12-26 23:05:47 +0100

    setup dotfiles

--- script-useful/architect.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo pacman -S --needed git base-devel && git clone https://github.com/Cardiacman13/Architect.git ~/Architect && cd ~/Architect && chmod +x ./architect.sh && ./architect.sh
